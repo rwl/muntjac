00ef097a7c78c27fb140bd763e8d9c3a2522309b
rwl <r.w.lincoln@gmail.com>
2011-09-25 00:28:28 +0100

    Updating test.

--- muntjac/test/run_app.py
+++ muntjac/test/run_app.py
@@ -1,9 +1,17 @@
+import webbrowser
+
 from os.path import dirname
 
 from paste.deploy import loadapp
 from paste.httpserver import serve
 
+
 wsgi_app = loadapp('config:conf.ini',
                    relative_to=dirname(__file__))
 
-serve(wsgi_app, host='127.0.0.1', port=8080)
+host = '127.0.0.1'
+port = 8080
+
+webbrowser.open('http://%s:%d/SimpleApp' % (host, port), new=0)
+
+serve(wsgi_app, host=host, port=port)
