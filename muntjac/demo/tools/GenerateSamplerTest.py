15980dbd6e5f891faf96f69daa86031ee73af924
rwl <r.w.lincoln@gmail.com>
2011-10-06 16:20:38 +0100

    First pass of GenerateSamplerTest.

--- muntjac/demo/tools/GenerateSamplerTest.py
+++ muntjac/demo/tools/GenerateSamplerTest.py
@@ -1,71 +1,94 @@
-# -*- coding: utf-8 -*-
-from com.vaadin.demo.sampler.FeatureSet import (FeatureSet,)
-# from java.util.ArrayList import (ArrayList,)
-# from java.util.List import (List,)
+
+try:
+    from cStringIO import StringIO
+except ImportError, e:
+    from StringIO import StringIO
+
+from muntjac.demo.sampler.FeatureSet import FeatureSet
+
+
+class TestRow(object):
+
+    def getHtml(self):
+        pass
+
+
+class Comment(TestRow):
+
+    def __init__(self, comment):
+        self._comment = comment
+
+    def getHtml(self):
+        return '<!--' + self._comment + '-->\n'
+
+
+class Command(TestRow):
+
+    def __init__(self, cmd, target, value):
+        super(Command, self)()
+        self._cmd = cmd
+        self._target = target
+        self._value = value
+
+    def getHtml(self):
+        return ('<tr>\n' + '<td>' + self._cmd + '</td>\n'
+            + '<td>' + self._target + '</td>\n' + '<td>'
+            + self._value + '</td>\n' + '</tr>\n')
+
+
+class WaitForVaadinCommand(Command):
+
+    def __init__(self):
+        super(WaitForVaadinCommand, self)('waitForVaadin', '', '')
+
+
+class VaadinTestBenchTest(object):
+
+    _header = ('<?xml version=\"1.0\" encoding=\"UTF-8\"?>'
+        + '<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">'
+        + '<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">'
+        + '<head profile=\"http://selenium-ide.openqa.org/profiles/test-case\">'
+        + '<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />'
+        + '<link rel=\"selenium.base\" href=\"\" />'
+        + '<title>#name#</title>'
+        + '</head>'
+        + '<body>'
+        + '<table cellpadding=\"1\" cellspacing=\"1\" border=\"1\">'
+        + '<thead>'
+        + '<tr><td rowspan=\"1\" colspan=\"3\">#name#</td></tr>'
+        + '</thead><tbody>')
+
+    _footer = '</tbody></table>' + '</body>' + '</html>'
+
+    def __init__(self, name):
+        self._name = name
+        self._rows = list()
+
+
+    def addCmd(self, cmd, target, value):
+        self._rows.add(Command(cmd, target, value))
+
+
+    def addComment(self, comment):
+        self._rows.add(Comment(comment))
+
+
+    def output(self):
+        print self._header.replace('#name#', self._name)
+        for row in self._rows:
+            print row.getHtml()
+        print self._footer.replace('#name#', self._name)
 
 
 class GenerateSamplerTest(object):
 
-    class VaadinTestBenchTest(object):
-        _header = '<?xml version=\"1.0\" encoding=\"UTF-8\"?>' + '<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">' + '<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\" lang=\"en\">' + '<head profile=\"http://selenium-ide.openqa.org/profiles/test-case\">' + '<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\" />' + '<link rel=\"selenium.base\" href=\"\" />' + '<title>#name#</title>' + '</head>' + '<body>' + '<table cellpadding=\"1\" cellspacing=\"1\" border=\"1\">' + '<thead>' + '<tr><td rowspan=\"1\" colspan=\"3\">#name#</td></tr>' + '</thead><tbody>'
-        _footer = '</tbody></table>' + '</body>' + '</html>'
-        _name = None
-        _rows = list()
-
-        def __init__(self, name):
-            self._name = name
-
-        def addCmd(self, cmd, target, value):
-            self._rows.add(self.Command(cmd, target, value))
-
-        class WaitForVaadinCommand(Command):
-
-            def __init__(self):
-                super(WaitForVaadinCommand, self)('waitForVaadin', '', '')
-
-        class TestRow(object):
-
-            def getHtml(self):
-                pass
-
-        class Comment(TestRow):
-            _comment = None
-
-            def __init__(self, comment):
-                self._comment = comment
-
-            def getHtml(self):
-                return '<!--' + self._comment + '-->\n'
-
-        class Command(TestRow):
-            _cmd = None
-            _target = None
-            _value = None
-
-            def __init__(self, cmd, target, value):
-                super(Command, self)()
-                self._cmd = cmd
-                self._target = target
-                self._value = value
-
-            def getHtml(self):
-                return '<tr>\n' + '<td>' + self._cmd + '</td>\n' + '<td>' + self._target + '</td>\n' + '<td>' + self._value + '</td>\n' + '</tr>\n'
-
-        def addComment(self, comment):
-            self._rows.add(self.Comment(comment))
-
-        def output(self):
-            print self._header.replace('#name#', self._name)
-            for row in self._rows:
-                sys.stdout.write(row.getHtml())
-            print self._footer.replace('#name#', self._name)
-
     _NEXT_BUTTON = 'vaadin=sampler::/VVerticalLayout[0]/ChildComponentContainer[0]/VHorizontalLayout[0]/ChildComponentContainer[6]/VHorizontalLayout[0]/ChildComponentContainer[1]/VNativeButton[0]'
+
     _test = None
 
     @classmethod
-    def main(cls, args):
-        cls._test = cls.VaadinTestBenchTest('sampler-all-samples')
+    def main(cls):
+        cls._test = VaadinTestBenchTest('sampler-all-samples')
         cls._test.addCmd('openAndWait', '/', '')
         cls._test.addComment('Open sampler from demo page')
         cls._test.addCmd('mouseClickAndWait', '//div[@id=\'sampler\']/a/strong', '96,14')
@@ -74,9 +97,9 @@ class GenerateSamplerTest(object):
         cls._test.addComment('Main page screenshot')
         cls._test.addCmd('pause', '5000', '')
         cls._test.addCmd('screenCapture', '', 'mainview')
-        script = cls.StringBuilder()
+        script = StringIO()
         cls._test.addComment('Scrolling through all samples to load images')
-        samplerFeatureSet = cls.com.vaadin.demo.sampler.FeatureSet.FEATURES
+        samplerFeatureSet = FeatureSet.FEATURES
         cls.writeFeatureSet(script, samplerFeatureSet, False)
         cls._test.addComment('Start over from the main page')
         cls._test.addCmd('mouseClick', 'vaadin=sampler::/VVerticalLayout[0]/ChildComponentContainer[0]/VHorizontalLayout[0]/ChildComponentContainer[1]/VCustomComponent[0]/VHorizontalLayout[0]/ChildComponentContainer[0]/VActiveLink[0]/domChild[0]/domChild[0]', '1,1')
@@ -96,25 +119,25 @@ class GenerateSamplerTest(object):
     def writeFeature(cls, script, feature, capture):
         cls._test.addCmd('mouseClick', cls._NEXT_BUTTON, '1,1')
         if capture:
-            id = cls.getId(feature)
-            cls._test.addComment(id)
-            if cls.includeScreenshotInTest(id):
+            idd = cls.getId(feature)
+            cls._test.addComment(idd)
+            if cls.includeScreenshotInTest(idd):
                 if cls.needsPause(feature):
                     cls._test.addCmd('pause', '1000', '')
                 if feature.getFragmentName().endswith('PackageIcons'):
                     # Firefox3, sometimes you disappoint me
                     cls._test.addCmd('pause', '3000', '')
-                cls._test.addCmd('screenCapture', '', id)
+                cls._test.addCmd('screenCapture', '', idd)
 
     @classmethod
-    def includeScreenshotInTest(cls, id):
-        if id.startswith('Date'):
+    def includeScreenshotInTest(cls, idd):
+        if idd.startswith('Date'):
             return False
-        if id == 'BrowserInformation':
+        if idd == 'BrowserInformation':
             return False
-        if id == 'WebEmbed':
+        if idd == 'WebEmbed':
             return False
-        if id == 'JSApi':
+        if idd == 'JSApi':
             return False
         return True
 
@@ -138,5 +161,4 @@ class GenerateSamplerTest(object):
 
 
 if __name__ == '__main__':
-    import sys
-    GenerateSamplerTest().main(sys.argv)
+    GenerateSamplerTest.main()
