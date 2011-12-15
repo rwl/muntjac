288e13339120b97116628ae410d41ed2761eed48
rwl <r.w.lincoln@gmail.com>
2011-12-15 01:13:13 +0000

    First pass of client.ui code.

--- muntjac/terminal/gwt/client/ui/v_option_group.py
+++ muntjac/terminal/gwt/client/ui/v_option_group.py
@@ -17,7 +17,192 @@
 # Note: This is a modified file from Vaadin. For further information on
 #       Vaadin please visit http://www.vaadin.com.
 
+from pyjamas.ui.CheckBox import CheckBox
+from pyjamas.ui.RadioButton import RadioButton
 
-class VOptionGroup(object):
+from muntjac.terminal.gwt.client.application_connection \
+    import ApplicationConnection
 
-    HTML_CONTENT_ALLOWED = "usehtml"
+from muntjac.terminal.gwt.client.ui.v_check_box import VCheckBox
+from muntjac.terminal.gwt.client.event_id import IEventId
+from muntjac.terminal.gwt.client.util import Util
+from muntjac.terminal.gwt.client.browser_info import BrowserInfo
+from muntjac.terminal.gwt.client.ui.v_option_group_base import VOptionGroupBase
+from muntjac.terminal.gwt.client.ui.icon import Icon
+
+
+class VOptionGroup(VOptionGroupBase):#, FocusHandler, BlurHandler):
+
+    HTML_CONTENT_ALLOWED = 'usehtml'
+
+    CLASSNAME = 'v-select-optiongroup'
+
+    def __init__(self):
+
+        self._panel = None
+        self._optionsToKeys = None
+        self._sendFocusEvents = False
+        self._sendBlurEvents = False
+        self._focusHandlers = None
+        self._blurHandlers = None
+
+        class _0_(LoadHandler):
+            def onLoad(self, event):
+                if (BrowserInfo.get().isIE6()):
+                    Util.doIE6PngFix((Element) Element._as(event.getNativeEvent().getEventTarget()))
+
+                Util.notifyParentOfSizeChange(VOptionGroup_this, True)
+
+        iconLoadHandler = _0_()
+
+        # used to check whether a blur really was a blur of the complete
+        # optiongroup: if a control inside this optiongroup gains focus right after
+        # blur of another control inside this optiongroup (meaning: if onFocus
+        # fires after onBlur has fired), the blur and focus won't be sent to the
+        # server side as only a focus change inside this optiongroup occured
+        _blurOccured = False
+
+        _htmlContentAllowed = False
+
+        super(VOptionGroup, self)(self.CLASSNAME)
+        self._panel = self.optionsContainer
+        self._optionsToKeys = dict()
+
+
+    def updateFromUIDL(self, uidl, client):
+        # Return true if no elements were changed, false otherwise.
+        self._htmlContentAllowed = uidl.hasAttribute(self.HTML_CONTENT_ALLOWED)
+
+        super(VOptionGroup, self).updateFromUIDL(uidl, client)
+
+        self._sendFocusEvents = client.hasEventListeners(self, IEventId.FOCUS)
+        self._sendBlurEvents = client.hasEventListeners(self, IEventId.BLUR)
+
+        if self._focusHandlers is not None:
+            for reg in self._focusHandlers:
+                reg.removeHandler()
+
+            self._focusHandlers.clear()
+            self._focusHandlers = None
+
+            for reg in self._blurHandlers:
+                reg.removeHandler()
+
+            self._blurHandlers.clear()
+            self._blurHandlers = None
+
+        if self._sendFocusEvents or self._sendBlurEvents:
+            self._focusHandlers = list()
+            self._blurHandlers = list()
+            # add focus and blur handlers to checkboxes / radio buttons
+            for wid in self._panel:
+                if isinstance(wid, CheckBox):
+                    self._focusHandlers.append(wid.addFocusHandler(self))
+                    self._blurHandlers.append(wid.addBlurHandler(self))
+
+
+    def buildOptions(self, uidl):
+        self._panel.clear()
+        _0 = True
+        it = uidl.getChildIterator()
+        while it.hasNext():
+            opUidl = it.next()
+
+            itemHtml = opUidl.getStringAttribute('caption')
+            if not self._htmlContentAllowed:
+                itemHtml = Util.escapeHTML(itemHtml)
+
+            icon = opUidl.getStringAttribute('icon')
+            if icon is not None and len(icon) != 0:
+                iconUrl = self.client.translateVaadinUri(icon)
+                itemHtml = ('<img src=\"' + iconUrl + '\" class=\"'
+                        + Icon.CLASSNAME + '\" alt=\"\" />' + itemHtml)
+
+            if self.isMultiselect():
+                op = VCheckBox()
+                op.setHTML(itemHtml)
+            else:
+                op = RadioButton(self.id, itemHtml, True)
+                op.setStyleName('v-radiobutton')
+
+            if icon is not None and len(icon) != 0:
+                Util.sinkOnloadForImages(op.getElement())
+                op.addHandler(self.iconLoadHandler, LoadEvent.getType())
+
+            op.addStyleName(self.CLASSNAME_OPTION)
+            op.setValue(opUidl.getBooleanAttribute('selected'))
+            enabled = (not opUidl.getBooleanAttribute('disabled')
+                    and not self.isReadonly() and not self.isDisabled())
+            op.setEnabled(enabled)
+            self.setStyleName(op.getElement(),
+                    ApplicationConnection.DISABLED_CLASSNAME, not enabled)
+            op.addClickHandler(self)
+            self._optionsToKeys.put(op, opUidl.getStringAttribute('key'))
+            self._panel.add(op)
+
+
+    def getSelectedItems(self):
+        return list(self.selectedKeys)
+
+
+    def onClick(self, event):
+        super(VOptionGroup, self).onClick(event)
+        if isinstance(event.getSource(), CheckBox):
+            selected = event.getSource().getValue()
+            key = self._optionsToKeys[event.getSource()]
+            if not self.isMultiselect():
+                self.selectedKeys.clear()
+
+            if selected:
+                self.selectedKeys.add(key)
+            else:
+                self.selectedKeys.remove(key)
+
+            self.client.updateVariable(self.id, 'selected',
+                    self.getSelectedItems(), self.isImmediate())
+
+
+    def setTabIndex(self, tabIndex):
+        iterator = self._panel
+        while iterator.hasNext():
+            widget = iterator.next()
+            widget.setTabIndex(tabIndex)
+
+
+    def focus(self):
+        iterator = self._panel
+        if iterator.hasNext():
+            iterator.next().setFocus(True)
+
+
+    def onFocus(self, arg0):
+        if not self._blurOccured:
+            # no blur occured before this focus event
+            # panel was blurred => fire the event to the server side if
+            # requested by server side
+            if self._sendFocusEvents:
+                self.client.updateVariable(self.id, IEventId.FOCUS, '', True)
+        else:
+            # blur occured before this focus event
+            # another control inside the panel (checkbox / radio box) was
+            # blurred => do not fire the focus and set blurOccured to false, so
+            # blur will not be fired, too
+            self._blurOccured = False
+
+
+    def onBlur(self, arg0):
+        self._blurOccured = True
+        if self._sendBlurEvents:
+
+            class _0_(Command):
+
+                def execute(self):
+                    # check whether blurOccured still is true and then send the
+                    # event out to the server
+                    if VOptionGroup_this._blurOccured:
+                        self.client.updateVariable(self.id, IEventId.BLUR,
+                                '', True)
+                        VOptionGroup_this._blurOccured = False
+
+            _0_ = _0_()
+            Scheduler.get().scheduleDeferred(_0_)
--- muntjac/terminal/gwt/client/ui/v_option_group_base.py
+++ muntjac/terminal/gwt/client/ui/v_option_group_base.py
@@ -17,107 +17,127 @@
 # Note: This is a modified file from Vaadin. For further information on
 #       Vaadin please visit http://www.vaadin.com.
 
-from __pyjamas__ import (ARGERROR,)
-from com.vaadin.terminal.gwt.client.Paintable import (Paintable,)
-from com.vaadin.terminal.gwt.client.ui.VTextField import (VTextField,)
-from com.vaadin.terminal.gwt.client.ui.VNativeButton import (VNativeButton,)
-from com.vaadin.terminal.gwt.client.ui.Field import (Field,)
-from com.vaadin.terminal.gwt.client.Focusable import (Focusable,)
-# from java.util.Set import (Set,)
+from pyjamas.ui.Composite import Composite
+from pyjamas.ui.FlowPanel import FlowPanel
+
+from muntjac.terminal.gwt.client.paintable import IPaintable
+from muntjac.terminal.gwt.client.ui.v_text_field import VTextField
+from muntjac.terminal.gwt.client.ui.v_native_button import VNativeButton
+from muntjac.terminal.gwt.client.ui.field import IField
+from muntjac.terminal.gwt.client.focusable import IFocusable
+from pyjamas.ui import KeyboardListener
 
 
-class VOptionGroupBase(Composite, Paintable, Field, ClickHandler, ChangeHandler, KeyPressHandler, Focusable):
+class VOptionGroupBase(Composite, IPaintable, IField, IFocusable):
+#            ClickHandler, ChangeHandler, KeyPressHandler):
+
     CLASSNAME_OPTION = 'v-select-option'
-    client = None
-    id = None
-    selectedKeys = None
-    _immediate = None
-    _multiselect = None
-    _disabled = None
-    _readonly = None
-    _cols = 0
-    _rows = 0
-    _nullSelectionAllowed = True
-    _nullSelectionItemAvailable = False
-    # Widget holding the different options (e.g. ListBox or Panel for radio
-    # buttons) (optional, fallbacks to container Panel)
 
-    optionsContainer = None
-    # Panel containing the component
-    _container = None
-    _newItemField = None
-    _newItemButton = None
-
-    def __init__(self, *args):
+    def __init__(self, widget_or_classname, classname=None):
         # Call this if you wish to specify your own container for the option
         # elements (e.g. SELECT)
 
-        _0 = args
-        _1 = len(args)
-        if _1 == 1:
-            classname, = _0
+        self.client = None
+        self.id = None
+        self.selectedKeys = None
+        self._immediate = None
+        self._multiselect = None
+        self._disabled = None
+        self._readonly = None
+        self._cols = 0
+        self._rows = 0
+        self._nullSelectionAllowed = True
+        self._nullSelectionItemAvailable = False
+
+        # Widget holding the different options (e.g. ListBox or Panel for radio
+        # buttons) (optional, fallbacks to container Panel)
+        self.optionsContainer = None
+
+        # Panel containing the component
+        self._container = None
+        self._newItemField = None
+        self._newItemButton = None
+
+        if classname is None:
+            classname = widget_or_classname
             self._container = FlowPanel()
             self.initWidget(self._container)
             self.optionsContainer = self._container
             self._container.setStyleName(classname)
             self._immediate = False
             self._multiselect = False
-        elif _1 == 2:
-            w, classname = _0
+        else:
+            w, classname = widget_or_classname, classname
             self.__init__(classname)
             self.optionsContainer = w
             self._container.add(self.optionsContainer)
-        else:
-            raise ARGERROR(1, 2)
+
 
     def isImmediate(self):
         return self._immediate
 
+
     def isMultiselect(self):
         return self._multiselect
+
 
     def isDisabled(self):
         return self._disabled
 
+
     def isReadonly(self):
         return self._readonly
+
 
     def isNullSelectionAllowed(self):
         return self._nullSelectionAllowed
 
+
     def isNullSelectionItemAvailable(self):
         return self._nullSelectionItemAvailable
+
 
     def getColumns(self):
         """@return "cols" specified in uidl, 0 if not specified"""
         return self._cols
 
+
     def getRows(self):
         """@return "rows" specified in uidl, 0 if not specified"""
         return self._rows
 
+
     def updateFromUIDL(self, uidl, client):
         self.client = client
         self.id = uidl.getId()
+
         if client.updateComponent(self, uidl, True):
             return
+
         self.selectedKeys = uidl.getStringArrayVariableAsSet('selected')
+
         self._readonly = uidl.getBooleanAttribute('readonly')
         self._disabled = uidl.getBooleanAttribute('disabled')
         self._multiselect = 'multi' == uidl.getStringAttribute('selectmode')
         self._immediate = uidl.getBooleanAttribute('immediate')
         self._nullSelectionAllowed = uidl.getBooleanAttribute('nullselect')
         self._nullSelectionItemAvailable = uidl.getBooleanAttribute('nullselectitem')
+
         if uidl.hasAttribute('cols'):
             self._cols = uidl.getIntAttribute('cols')
+
         if uidl.hasAttribute('rows'):
             self._rows = uidl.getIntAttribute('rows')
+
         ops = uidl.getChildUIDL(0)
+
         if self.getColumns() > 0:
             self._container.setWidth(self.getColumns() + 'em')
             if self._container != self.optionsContainer:
                 self.optionsContainer.setWidth('100%')
+
         self.buildOptions(ops)
+
         if uidl.getBooleanAttribute('allownewitem'):
             if self._newItemField is None:
                 self._newItemButton = VNativeButton()
@@ -125,47 +145,59 @@ class VOptionGroupBase(Composite, Paintable, Field, ClickHandler, ChangeHandler,
                 self._newItemButton.addClickHandler(self)
                 self._newItemField = VTextField()
                 self._newItemField.addKeyPressHandler(self)
+
             self._newItemField.setEnabled(not self._disabled and not self._readonly)
             self._newItemButton.setEnabled(not self._disabled and not self._readonly)
-            if (
-                (self._newItemField is None) or (self._newItemField.getParent() != self._container)
-            ):
+
+            if ((self._newItemField is None)
+                    or (self._newItemField.getParent() != self._container)):
                 self._container.add(self._newItemField)
                 self._container.add(self._newItemButton)
-                w = self._container.getOffsetWidth() - self._newItemButton.getOffsetWidth()
+                w = (self._container.getOffsetWidth()
+                        - self._newItemButton.getOffsetWidth())
                 self._newItemField.setWidth(self.Math.max(w, 0) + 'px')
+
         elif self._newItemField is not None:
             self._container.remove(self._newItemField)
             self._container.remove(self._newItemButton)
+
         self.setTabIndex(uidl.getIntAttribute('tabindex') if uidl.hasAttribute('tabindex') else 0)
 
+
     def setTabIndex(self, tabIndex):
-        pass
+        raise NotImplementedError
+
 
     def onClick(self, event):
-        if (
-            event.getSource() == self._newItemButton and not (self._newItemField.getText() == '')
-        ):
-            self.client.updateVariable(self.id, 'newitem', self._newItemField.getText(), True)
+        if (event.getSource() == self._newItemButton
+                and not (self._newItemField.getText() == '')):
+            self.client.updateVariable(self.id, 'newitem',
+                    self._newItemField.getText(), True)
             self._newItemField.setText('')
+
 
     def onChange(self, event):
         if self._multiselect:
-            self.client.updateVariable(self.id, 'selected', self.getSelectedItems(), self._immediate)
+            self.client.updateVariable(self.id, 'selected',
+                    self.getSelectedItems(), self._immediate)
         else:
-            self.client.updateVariable(self.id, 'selected', ['' + self.getSelectedItem()], self._immediate)
+            self.client.updateVariable(self.id, 'selected',
+                    [str(self.getSelectedItem())], self._immediate)
+
 
     def onKeyPress(self, event):
-        if (
-            event.getSource() == self._newItemField and event.getCharCode() == KeyCodes.KEY_ENTER
-        ):
+        if (event.getSource() == self._newItemField
+                and event.getCharCode() == KeyboardListener.KEY_ENTER):
             self._newItemButton.click()
 
+
     def buildOptions(self, uidl):
-        pass
+        raise NotImplementedError
+
 
     def getSelectedItems(self):
-        pass
+        raise NotImplementedError
+
 
     def getSelectedItem(self):
         sel = self.getSelectedItems()
